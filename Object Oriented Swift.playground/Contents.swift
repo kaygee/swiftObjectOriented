1cb8bf5c539e0db5a1c8e797073952267a6d9b55
Kevin Gann <kgann@5amsolutions.com>
2016-07-16 08:52:37 -0500

    Adding classes and methods.

--- Object Oriented Swift.playground/Contents.swift
+++ Object Oriented Swift.playground/Contents.swift
@@ -81,4 +81,81 @@ struct RGBColor {
 let color = RGBColor(red: 1.0, green: 2.0, blue: 3.0, alpha: 4.0)
 color.description
 
+class Enemy {
+    var life: Int = 2
+    var position: Point
+    
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func decreaseHealth(factor: Int) {
+        life -= factor
+    }
+}
 
+class Shape {
+    var numberOfSides: Int
+    
+    init(numberOfSides: Int) {
+        self.numberOfSides = numberOfSides
+    }
+}
+
+let someShape = Shape(numberOfSides: 3)
+
+class Tower {
+    let position: Point
+    var range: Int = 1
+    var strength: Int = 1
+    
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func fireAtEnemy(enemy: Enemy) {
+        if (inRange(position, range: self.range, target: enemy.position)) {
+            while enemy.life > 0 {
+                enemy.decreaseHealth(self.strength)
+                print("Enemy in range! Firing!")
+            }
+            print("Enemy vanquished!")
+        } else {
+            print("Enemy out of range!")
+        }
+    }
+    
+    func inRange(position: Point, range: Int, target: Point) -> Bool {
+        let availablePositions = position.getSurroundingPoints(withRange: range)
+        for point in availablePositions {
+            if (point.x == target.x) && (point.y == target.y) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+let tower = Tower(x: 0, y: 0)
+let enemy = Enemy(x: 1, y: 1)
+let farAwayEnemy = Enemy(x: 5, y: 5)
+
+tower.fireAtEnemy(enemy)
+tower.fireAtEnemy(farAwayEnemy)
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+}
+
+class Business {
+    let name: String
+    let location: Location
+    
+    init(name: String, location: Location) {
+        self.name = name
+        self.location = location
+    }
+}
+
+let someBusiness = Business(name: "5AM", location: Location(latitude: 1.0, longitude: 3.0))
